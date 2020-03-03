a9fecd5ecbb5ed3ada1adb15fc109783cdbf1017
TARUC <tan.kaixiang27@gmail.com>
2020-03-03 16:01:29 +0800

    commit

---
+++ app/src/main/java/com/example/practical_1/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.practical_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Button view from the layout and assign a click
+        // listener to it.
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.countup_button)
+        countButton.setOnClickListener { countUp() }
+    }
+
+    /**
+     * Click listener for the Roll button.
+     */
+    private fun rollDice() {
+        val randomInt= (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+    /**
+     * Click listener for the countUp button.
+     */
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        // If text is the default "Hello World!" set that text to 1.
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            // Otherwise, increment the number up to 6.
+            // The text value in resultText.text is an instance of the CharSequence class;
+            // it needs to be converted to a String object before it can be converted to an int.
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+    }
+}
